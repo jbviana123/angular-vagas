f19344b02c5ee42641e379b3711fb67fca08eaf4
jbvianna123 <joaobvianna@hotmail.com>
2022-01-21 04:39:09 -0300

    criando services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
 
@@ -12,7 +12,8 @@ import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
